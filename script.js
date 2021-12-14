cee3fef749f95ae164d3054d40a0c385bb097da6
koltrastH <elina.hulbert@hyperisland.se>
2021-12-14 18:18:54 +0100

    adding the src of audio

--- script.js
+++ script.js
@@ -1,31 +1,61 @@
-fetch('https://api.sr.se/api/v2/channels?format=json')
+fetch('https://api.sr.se/api/v2/channels?format=json&indent=true&pagination=false')
   .then(response => response.json())
   .then(data => showTheTitle(data));
  
 
+  let currentlyActive
+
  function showTheTitle(data){
+   console.log(data)
+   const tracks = document.getElementById("tracks")
 
-    for (i = 0; i<data.channels.length; i++){
-    console.log(data.channels[i])
+
+   for (i = 0; i<data.channels.length; i++){
+    // console.log(data.programs[i])
+
+// IMAGE
+    const playlistItem = document.createElement('div');
+    playlistItem.classList.add('playlist-item')
+
+    const playlistItemImg = document.createElement('img')
+    playlistItemImg.classList.add('playlist-item-img')
+    playlistItemImg.setAttribute('src', data.channels[i].image.toString())
+   //  console.log(playlistItemImg)
+
+   //  document.getElementById("cover-image").src=data.channels[6].image
+   tracks.appendChild(playlistItem);
+   playlistItem.appendChild(playlistItemImg);
+//AUDIO
+    // const audioSrc = document.getElementById("mp3")
+    // audioSrc.setAttribute("src", data.channels[i].liveaudio.url)
+   
+    const audioPlayer = document.getElementById("player")
+    playlistItem.addEventListener('click', () =>{
+        if(currentlyActive === data.channels[i].liveaudio){
+          audioPlayer.pause()
+          currentlyActive=null;
+          playlistItem.classList.remove('active')
+        } else {
+          if(currentlyActive){
+            document
+            .querySelector('.active').classList.remove('active')
+          }
+          currentlyActive = data.channels[i].liveaudio
+          playlistItem.classList.add('active')
+        //Play preview if available
+          if(data.channels[i].liveaudio){
+            audioPlayer.setAttribute('src', data.channels[i].liveaudio.url.toString())
+            audioPlayer.play()
+          } else{
+            audioPlayer.pause()
+          }
+          console.log(audioSrc)
+        }
+      })
+      
    }
+  
+   
+  
+
 }
-
-// const arrayOfId=[132, 163, 164, 213, 223, 205, 210, 212, 220, 200]
-// for (const id of arrayOfId){
-//     const response = await fetch('https://api.sr.se/api/v2/channels?format=json')
-// }
-
-//132 P1
-//163 P2
-//164 P3
-//213 P4 Blekinge
-//223 P4 Dalarna
-//205 P4 Gotland
-//210 P4 Gävleborg
-//212 P4 Göteborg
-//220 P4 Halland
-//200 P4 Jämtland
-
-
-
-
